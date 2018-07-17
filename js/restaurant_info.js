f564e79330ea112caa9b8df5b54e4653905ad238
Paul Standley <paulstandley1972@gmail.com>
2018-07-17 16:29:02 +0100

    new style sheet

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoicGF1bHN0YW5kbGV5IiwiYSI6ImNqamZzcjRodjUydmszcG1tb25jOWR6aXAifQ.hlOSJDuO14yBQ2hwNWZm8Q',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
